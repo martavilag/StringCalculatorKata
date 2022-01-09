b0ff4d95d545e7d74e447cd37f9a8bf28b0e0081
mvila <mvila@voxelgroup.net>
2022-01-09 11:37:22 +0100

    feat: returns one when string is one

--- StringCalculatorKata2/StringCalculator.cs
+++ StringCalculatorKata2/StringCalculator.cs
@@ -1,14 +1,20 @@
-﻿namespace StringCalculatorKata2
+﻿using System;
+
+namespace StringCalculatorKata2
 {
     public class StringCalculator
     {
-        public StringCalculator(string empty)
+        private readonly string _sequence;
+
+        public StringCalculator(string sequence)
         {
-            
+            _sequence = sequence;
         }
 
         public int Sum()
         {
+            if (_sequence == "1")
+                return Convert.ToInt32(_sequence);
             return 0;
         }
     }
