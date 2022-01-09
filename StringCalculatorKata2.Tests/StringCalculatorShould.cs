730ac054695e79c9f33e4146786c6cb8b6fc2dd3
mvila <mvila@voxelgroup.net>
2022-01-09 11:32:49 +0100

    test: return one when string is one

--- StringCalculatorKata2.Tests/StringCalculatorShould.cs
+++ StringCalculatorKata2.Tests/StringCalculatorShould.cs
@@ -11,5 +11,12 @@ namespace StringCalculatorKata2.Tests
             var emptyString = new StringCalculator("");
             emptyString.Sum().Should().Be(0);
         }
+
+        [Fact]
+        public void ReturnOneWhenStringIsOne()
+        {
+            var stringOne = new StringCalculator("1");
+            stringOne.Sum().Should().Be(1);
+        }
     }
 }
